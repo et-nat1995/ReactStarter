b4ab3de7592c1e776c0ae434e6d97ea73edf9285
Tamir <t.natanov@gymnasticbodies.com>
2019-10-23 08:13:07 -0700

    Added live microservice api

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -5,15 +5,15 @@ export default {
     return Axios.get("/api/success");
   },
   submitConversion: ({from, to, amount}) => {
-    return Axios.post(`/currency-conversion-history-service/from/${from}/to/${to}/quantity/${amount}`);
+    return Axios.post(`/my-currency-conversion-history-service/from/${from}/to/${to}/quantity/${amount}`);
   },
   getHistory: () => {
-    return Axios.get('/currency-conversion-history-service/lists');
+    return Axios.get('/my-currency-conversion-history-service/lists');
   },
   deletRow: ({ id })=>{
-    return Axios.delete(`/currency-conversion-history-service/lists/${id}`);
+    return Axios.delete(`/my-currency-conversion-history-service/lists/${id}`);
   },
   updateRow: (body) => {
-    return Axios.put('/currency-conversion-history-service/lists', body);
+    return Axios.put('/my-currency-conversion-history-service/lists', body);
   }
 }
