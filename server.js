2ffdc31665a38d077f912677aadcd18a320db6c2
Tamir <t.natanov@gymnasticbodies.com>
2019-10-24 09:47:33 -0700

    Pushing to heroku to test a build launch

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require("path")
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,13 +16,23 @@ const mongoURI = process.env.MONGODB_URI /* || place dev uri here */;
 
 const app = express();
 app.use(morgan("dev"));
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 app.use(require("./routes"));
 
 // allows all static files to be loaded and used by the server.
 app.use(express.static("public"));
 // builds the req.body
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 app.listen(PORT, function () {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
